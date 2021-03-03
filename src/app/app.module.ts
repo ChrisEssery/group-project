14cc4452e0bfc4a6423c5bccf8fcdd846f949c40
lizhao <72632137+lizhaoliu97@users.noreply.github.com>
2021-03-03 14:33:36 +0000

    add a start page
    Update:
    - add a start page template with three components: background music(.mp3 file stored /src/assets/audio, website title, and page background
    
    Problem:
    - audio "autoplay" attribute doesn't work in Safari and Chrome browser
    
    Next step:
    - add a play/pause button so that the user can choose to play and stop the audioto deal with the problem
    - using typescript to do with the logic about controlling the play/pause button

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StartPageComponent } from './start-page/start-page.component';
+import { TitleComponent } from './start-page/title/title.component';
+import { BackgroundComponent } from './start-page/background/background.component';
+import { BgmComponent } from './start-page/bgm/bgm.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StartPageComponent,
+    TitleComponent,
+    BackgroundComponent,
+    BgmComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/start-page/bgm/bgm.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bgm',
+  templateUrl: './bgm.component.html',
+  styleUrls: ['./bgm.component.css']
+})
+export class BgmComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
