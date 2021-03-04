d52e611b350ebb26cd4024d863e8d27a6d5c2963
lizhao <72632137+lizhaoliu97@users.noreply.github.com>
2021-03-04 21:54:52 +0000

    fix the problem about music autoplay
    adding a button to play/pause the bgm

--- src/app/start-page/bgm/bgm.component.ts
+++ src/app/start-page/bgm/bgm.component.ts
@@ -1,13 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-bgm',
   templateUrl: './bgm.component.html',
-  styleUrls: ['./bgm.component.css']
+  styleUrls: ['./bgm.component.css'],
 })
 export class BgmComponent implements OnInit {
+  public bgm = new Audio("../../../assets/audio/Sunshine_Kiss.mp3");
+  constructor() {
+   }
 
-  constructor() { }
+  controlbgm(){
+    if(this.bgm.paused){
+      console.log("trying to play bgm");
+      this.bgm.play();
+      this.bgm.loop = true;
+    }else{
+      console.log("trying to pause bgm");
+      this.bgm.pause();
+    }
+  }
 
   ngOnInit(): void {
   }
