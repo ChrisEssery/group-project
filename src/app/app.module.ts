06a22f454fc8eca5d908586d465551d7ca0dd8bf
Chris Essery <christopher.essery@gmail.com>
2021-03-18 12:26:26 +0000

    connect 4 app.module update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { StartPageComponent } from './start-page/start-page.component';
 import { TitleComponent } from './start-page/title/title.component';
 import { BackgroundComponent } from './start-page/background/background.component';
 import { BgmComponent } from './start-page/bgm/bgm.component';
+import { ConnectFourComponent } from './connect-four/connect-four.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { BgmComponent } from './start-page/bgm/bgm.component';
     StartPageComponent,
     TitleComponent,
     BackgroundComponent,
-    BgmComponent
+    BgmComponent,
+    ConnectFourComponent
   ],
   imports: [
     BrowserModule,
