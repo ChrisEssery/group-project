954b7cc800efe3edc5af7ff0375aa9f88e911106
MaxSkippGriff <mskippergriffiths@gmail.com>
2021-04-12 09:44:31 +0100

    :wrench: editing routes to redirect to memory game login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { StartComponent } from "./route/start-login/start-login.component";
 
-const routes: Routes = [];
+const routes: Routes = [];const routes: Routes = [
+  { path: "", redirectTo: "/start-login", pathMatch: "full" },
+  { path: "start-login", component: StartComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
