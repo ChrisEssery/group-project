541416a1ef8b037f4f104e38f4ea4082030d3f4a
MaxSkippGriff <mskippergriffiths@gmail.com>
2021-04-12 17:14:19 +0100

    :wrench: removing login page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,9 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
-import { StartLoginComponent } from "./route/start-login/start-login.component";
 import { GameplayComponent } from "./route/gameplay/gameplay.component";
 
 const routes: Routes = [
-  { path: "", redirectTo: "/start", pathMatch: "full" },
-  { path: "start", component: StartLoginComponent },
+  { path: "", redirectTo: "/playing", pathMatch: "full" },
   { path: "playing", component: GameplayComponent }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { CardsComponent } from './memory-components/cards/cards.component';
-import { StartLoginComponent } from './route/start-login/start-login.component';
 import { GameplayComponent } from './route/gameplay/gameplay.component';
 import { MemoryGameComponent } from './memory-components/memory-game/memory-game.component';
 import { ButtonComponent } from './memory-components/button/button.component';
@@ -16,7 +15,6 @@ import { HeadingComponent } from './memory-components/heading/heading.component'
   declarations: [
     AppComponent,
     CardsComponent,
-    StartLoginComponent,
     GameplayComponent,
     MemoryGameComponent,
     ButtonComponent,
