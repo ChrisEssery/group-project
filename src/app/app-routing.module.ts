7eeb73411e35bdf333a1e878ca6679c987b9c2b3
MaxSkippGriff <mskippergriffiths@gmail.com>
2021-04-12 10:26:00 +0100

    editing styles and debugging routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { StartComponent } from "./route/start-login/start-login.component";
+import { StartLoginComponent } from "./route/start-login/start-login.component";
 
 const routes: Routes = [
   { path: "", redirectTo: "/start-login", pathMatch: "full" },
