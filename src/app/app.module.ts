ff96045463e9c60dd2acbbfbf2c1314254330404
MaxSkippGriff <mskippergriffiths@gmail.com>
2021-04-12 14:20:17 +0100

    :wrench: edit tsconfig.json to silence errors

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,24 +4,16 @@ import { FormsModule } from "@angular/forms";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { StartPageComponent } from './start-page/start-page.component';
-import { TitleComponent } from './start-page/title/title.component';
-import { BackgroundComponent } from './start-page/background/background.component';
-import { BgmComponent } from './start-page/bgm/bgm.component';
-import { MemoryGameComponent } from './memory-components/memory-game/memory-game.component';
 import { StartLoginComponent } from './route/start-login/start-login.component';
+import { GameplayComponent } from './gameplay/gameplay.component';
+import { MemoryGameComponent } from './memory-components/memory-game/memory-game.component';
 import { ButtonComponent } from './memory-components/button/button.component';
 import { CardsComponent } from './memory-components/cards.component';
 import { HeadingComponent } from './heading/heading.component';
-import { GameplayComponent } from './gameplay/gameplay.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    StartPageComponent,
-    TitleComponent,
-    BackgroundComponent,
-    BgmComponent,
     MemoryGameComponent,
     StartLoginComponent,
     ButtonComponent,
