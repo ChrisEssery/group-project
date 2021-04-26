bf773d03ac319bd248a0b31610f4a1d2ee58f82f
lizhao <72632137+lizhaoliu97@users.noreply.github.com>
2021-04-26 15:16:42 +0100

    add profile page

--- src/app/home-page/profile/profile.component.ts
+++ src/app/home-page/profile/profile.component.ts
@@ -7,10 +7,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
+  friends=[
+    "aaa",
+    "bbb",
+    "ccc",
+    "ddd",
+    "eee",
+    "fff"
+]
+games:any=[
+  {
+      "playedWith": [
+          "bbb"
+      ],
+      "_id": "6080bfd26216e5aaeee2049d",
+      "gamename": "Memory Game",
+      "result": "win",
+      "date": "2021-04-22T00:14:10.968Z"
+  },
+  {
+      "playedWith": [
+          "bbb"
+      ],
+      "_id": "6080bfff6216e5aaeee204a2",
+      "gamename": "Connect 4",
+      "result": "win",
+      "date": "2021-04-22T00:14:55.965Z"
+  }
+]
 
   constructor(
   ) {}
 
   ngOnInit(): void {
+    this.games.forEach((element:any) => {
+      element.date = element.date.substring(5, 10)
+    });
   }
 }
