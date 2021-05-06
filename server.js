0b91178ec507c69d9320509ba0aba0f0beccf48b
Chris Essery <christopher.essery@gmail.com>
2021-05-06 20:23:25 +0100

    Added memory game server code

--- server.js
+++ server.js
@@ -133,3 +133,118 @@ Socketio.on('connection', socket => {
     })
 });
 
+// MEMORY GAME 
+const Express_mg = require("express")();
+const Http_mg = require("http").Server(Express_mg);
+const Socketio_mg = require ("socket.io")(Http_mg);
+
+Http.listen(3050, () => {
+    console.log("Listening at :3050...");
+});
+
+// Handle connection request
+const connections_mg = [null, null]
+let cardDeck = [];
+
+Socketio_mg.on('connection', socket => {
+    console.log('New connection');
+    // Find available player numbers
+    let playerIndex_mg = -1;
+    for(const i in connections_mg) {
+        if(connections_mg[i] === null){
+            playerIndex_mg = i;
+            break;
+        }
+    }
+
+    socket.emit('player-number', playerIndex);
+    console.log(`Player ${playerIndex} has connected`);
+    if(playerIndex === -1){
+        return;
+    }
+
+    connections_mg[playerIndex] = false;
+
+    // Tell everyone what player number just connected
+    socket.broadcast.emit('player-connection', playerIndex);
+
+    socket.on('disconnect', () => {
+        console.log(`Player ${playerIndex} disconnected`);
+        connections_mg[playerIndex] = null;
+        //Tell what player disconnected
+        socket.broadcast.emit('player-connection', playerIndex);
+    });
+
+    // On ready
+    socket.on('player-ready', () => {
+        socket.broadcast.emit('opponent-ready', playerIndex);
+        connections_mg[playerIndex] = true;
+    })
+
+    // Check player connections
+    socket.on('check-players', () => {
+        const players = []
+        for(const i in connections_mg) {
+            connections_mg[i] === null ? players.push({connected: false, ready: false}) : 
+            players.push({connected: true, ready: connections_mg[i]});
+            socket.emit('check-players', players);
+        }
+    })
+
+    // On slot received
+    socket.on('card-flipped', card => {
+        console.log(`Turn taken by ${playerIndex}`, card);
+
+        // Emit move to the other player
+        socket.broadcast.emit('card-flipped', card);
+    })
+
+    socket.on('send-card', card => {
+        cardDeck.push(card);
+    })
+
+    socket.on('card-request', () => {
+        console.log("Lengthyyyy: " + cardDeck.length);
+        for(let i = 0; i < cardDeck.length; i++){
+            console.log("Sending...")
+        socket.emit('card-sent', cardDeck[i]);
+        }
+    })
+
+    socket.on('card-request-again', () => {
+        console.log(cardDeck.length);
+        for(let i = 0; i < cardDeck.length; i++){
+            console.log("Sending...")
+        socket.emit('card-sent-again', cardDeck[i]);
+        }
+    })
+
+    // Clears deck when starting another game
+    socket.on('clear-deck', () => {
+        cardDeck = [];
+    })
+
+    let playAgain = true;
+
+    socket.on('play-again', () => {
+        socket.broadcast.emit('play-again', playAgain);
+    })
+
+    // On slot reply
+    socket.on('slot-reply', id => {
+        console.log(id);
+
+        // Forward to the other player
+        socket.broadcast.emit('slot-reply', id);
+    })
+
+    socket.on('game-over', () => {
+        socket.broadcast.emit('game-over', true);
+    })
+
+    // When game ready to start
+    socket.on('game-start', () => {
+        console.log("going");
+        socket.emit('ready-to-go', true);
+    })
+});
