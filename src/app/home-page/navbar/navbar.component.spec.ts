b1292e9ef0168739279af0150eb97b89e22aef40
Harri Grace <har.grace@yahoo.co.uk>
2021-05-06 11:52:47 +0100

    Adding testing components.

--- src/app/home-page/navbar/navbar.component.spec.ts
+++ src/app/home-page/navbar/navbar.component.spec.ts
@@ -2,6 +2,9 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { HttpClientModule } from '@angular/common/http';
 import { NavbarComponent } from './navbar.component';
 import { RouterTestingModule } from '@angular/router/testing';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+import { tick } from '@angular/core/testing';
 
 describe('NavbarComponent', () => {
   let component: NavbarComponent;
@@ -24,4 +27,17 @@ describe('NavbarComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  //it('should navigate', () => {
+  //  expect(DebugElement.query(By.css('#home')).nativeElement.attribute('routerLink')).toEqual('/home');
+  //});
+
+  it('should signout when signout is clicked', fakeAsync ((); void => {
+    spyOn(component, 'signout');
+    const signoutLink = fixture.debugElement.query(By.css('#signout-link'));
+    signoutLink.triggerEventHandler('click', null);
+    tick();
+    fixture.detectChanges();
+    expect(component.signout).toHaveBeenCalled();
+  }));
 });
--- src/app/login-page/login.component.spec.ts
+++ src/app/login-page/login.component.spec.ts
@@ -3,10 +3,13 @@ import { HttpClientModule } from '@angular/common/http';
 import { LoginComponent } from './login.component';
 import { RouterTestingModule } from '@angular/router/testing';
 import { FormsModule } from '@angular/forms';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
+  let html: DebugElement;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -24,5 +27,14 @@ describe('LoginComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('invalid username is hidden', () => {
+    expect(html.query(By.css('#invalidUsername')).nativeElement.attributes('hidden')).toBeTruthy();
+  });
+
+
+  it('invalid password is hidden', () => {
+    expect(html.query(By.css('#invalidPassword')).nativeElement.hasAttribute('hidden')).toBeTruthy();
   });
 });
