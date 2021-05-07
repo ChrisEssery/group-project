01fe7c78f6450f4d8605ee5441598563d798199b
Harri Grace <har.grace@yahoo.co.uk>
2021-05-07 12:47:41 +0100

    Fixing test files

--- src/app/service/game.service.spec.ts
+++ src/app/service/game.service.spec.ts
@@ -1,9 +1,11 @@
 import { TestBed } from '@angular/core/testing';
-
+import { RouterTestingModule } from '@angular/router/testing';
 import { GameService } from './game.service';
 
 describe('GameService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [ RouterTestingModule ]
+  }));
 
   it('should be created', () => {
     const service: GameService = TestBed.get(GameService);
