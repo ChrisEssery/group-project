c80b49714c6432e88110e2fa3e663f2712d1b219
Harri Grace <har.grace@yahoo.co.uk>
2021-05-07 12:25:09 +0100

    Fixing testing components

--- src/app/memory-components/game-window/game-window.component.spec.ts
+++ src/app/memory-components/game-window/game-window.component.spec.ts
@@ -1,13 +1,12 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { GameWindowComponent } from './game-window.component';
 
 describe('GameWindowComponent', () => {
   let component: GameWindowComponent;
   let fixture: ComponentFixture<GameWindowComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
+  beforeEach(async() => {
+    await TestBed.configureTestingModule({
       declarations: [ GameWindowComponent ]
     })
     .compileComponents();
