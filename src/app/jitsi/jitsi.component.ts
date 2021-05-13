ef50a0579816df3a31d7d8d86f6408d4eb8a898f
Chris Essery <christopher.essery@gmail.com>
2021-05-13 15:39:38 +0100

    Cleaning Jitsi code

--- src/app/jitsi/jitsi.component.ts
+++ src/app/jitsi/jitsi.component.ts
@@ -9,24 +9,20 @@ declare var JitsiMeetExternalAPI: any;
 })
 
 export class JitsiComponent implements OnInit, AfterViewInit {
-  domain: string = "meet.jit.si"; // For self hosted use your domain
+  domain: string = "meet.jit.si"; 
   room: any;
   options: any;
   api: any;
   user: any;
-
-  // For Custom Controls
-  isAudioMuted = false;
-  isVideoMuted = false;
 
   constructor(
       private router: Router
   ) { }
 
   ngOnInit(): void {
-      this.room = 'NotSoBoredGames'; // Set your room name
+      this.room = 'NotSoBoredGames';
       this.user = {
-          name: 'Placeholder' // Set your username
+          name: 'Placeholder'
       }
   }
 
@@ -63,17 +59,16 @@ export class JitsiComponent implements OnInit, AfterViewInit {
 }
 
 handleParticipantLeft = async (participant) => {
-    console.log("handleParticipantLeft", participant); // { id: "2baa184e" }
+    console.log("handleParticipantLeft", participant);
     const data = await this.getParticipants();
 }
 
 handleParticipantJoined = async (participant) => {
-    console.log("handleParticipantJoined", participant); // { id: "2baa184e", displayName: "Shanu Verma", formattedDisplayName: "Shanu Verma" }
-    const data = await this.getParticipants();
+    console.log("handleParticipantJoined", participant);
 }
 
 handleVideoConferenceJoined = async (participant) => {
-    console.log("handleVideoConferenceJoined", participant); // { roomName: "bwb-bfqi-vmh", id: "8c35a951", displayName: "Akash Verma", formattedDisplayName: "Akash Verma (me)"}
+    console.log("handleVideoConferenceJoined", participant);
     const data = await this.getParticipants();
 }
 
@@ -83,33 +78,18 @@ handleVideoConferenceLeft = () => {
 }
 
 handleMuteStatus = (audio) => {
-    console.log("handleMuteStatus", audio); // { muted: true }
+    console.log("handleMuteStatus", audio);
 }
 
 handleVideoStatus = (video) => {
-    console.log("handleVideoStatus", video); // { muted: true }
+    console.log("handleVideoStatus", video);
 }
 
 getParticipants() {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
-            resolve(this.api.getParticipantsInfo()); // get all participants
+            resolve(this.api.getParticipantsInfo());
         }, 500)
     });
-}
-executeCommand(command: string) {
-  this.api.executeCommand(command);;
-  if(command == 'hangup') {
-      this.router.navigate(['/thank-you']);
-      return;
-  }
-
-  if(command == 'toggleAudio') {
-      this.isAudioMuted = !this.isAudioMuted;
-  }
-
-  if(command == 'toggleVideo') {
-      this.isVideoMuted = !this.isVideoMuted;
-  }
 }
 }
